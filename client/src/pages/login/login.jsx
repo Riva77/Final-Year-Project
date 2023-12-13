09f95506e45e42302058a8316cdd58c9d9806bc3
Riva Karki <rivakarki86@gmail.com>
2023-12-13 12:28:50 +0545

    Try

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const login = () => {
-  return (
-    <div>login</div>
-  )
-}
+  return <div>Riya</div>;
+};
 
-export default login
+export default login;
