7ff416836674529aed1f9dacfb88788c7a7394b6
Riva Karki <rivakarki86@gmail.com>
2023-12-21 20:54:40 +0545

    Login page

---
+++ client/src/components/inputField/TextInput.jsx
@@ -0,0 +1,91 @@
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
+
+const TextInput = ({ type, value, onChange, name, label }) => {
+  const [inputType, setInputType] = useState(type);
+
+  const handlePasswordVisibility = () => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+
+  return (
+    <div style={{ position: "relative", width: 260 }}>
+      <label
+        htmlFor={name}
+        style={{
+          minWidth: "260px",
+          display: "block",
+          overflow: "hidden",
+          borderRadius: "8px",
+          border: "2px solid  #e5e7eb",
+          padding: "6px 8px",
+          width: "100%",
+          boxShadow: "0 2px 3px rgba(0, 0, 0, 0.05)",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "14px",
+            fontWeight: "500",
+            color: "black",
+          }}
+        >
+          {label}
+        </span>
+
+        <input
+          type={inputType}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={label}
+          style={{
+            marginTop: "4px",
+            width: "100%",
+            border: "none",
+            background: "transparent",
+            color: "#000000",
+            padding: "0",
+            outline: "none",
+          }}
+        />
+
+        {type === "password" && (
+          <span
+            style={{
+              position: "absolute",
+              top: "60%",
+              right: "3px",
+              transform: "translateY(-50%)",
+              cursor: "pointer",
+            }}
+            onClick={handlePasswordVisibility}
+          >
+            {inputType === "password" ? (
+              <AiOutlineEyeInvisible
+                size={20}
+                style={{
+                  color: "black",
+                }}
+              />
+            ) : (
+              <AiOutlineEye
+                size={20}
+                style={{
+                  color: "black",
+                }}
+              />
+            )}
+          </span>
+        )}
+      </label>
+    </div>
+  );
+};
+
+export default TextInput;
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import { useState } from "react";
+import background from "../../assets/background.jpg";
+import { CustomButton, TextInput } from "../../components";
+import "../../styles/login.css";
 
-const login = () => {
-  return <div>Riya</div>;
+const Login = () => {
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch(); //reducer use garera store ma data set garna help garxa
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleForgotPassword = async () => {
+    const email = formData.email;
+    if (email === "") {
+      toastError("Email field is empty!");
+    } else {
+      const OTP = Math.floor(Math.random() * 9000 + 1000);
+      dispatch(setOTP(OTP));
+    }
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // const response = await login(formData);
+
+    // if (response.success) {
+    //   const userToken = response.data.token;
+    //   // console.log(jwtDecode(userToken));
+    //   localStorage.setItem("userToken", userToken); //Key= "userToken", userToken= value. Aba get garni bela key matra chainxa
+    //   toastSuccess("Logged in successfully!");
+    //   navigate("/");
+    // } else {
+    //   toastError(response.error);
+    // }
+    // console.log("response", response);
+  };
+  return (
+    <div className="container">
+      <section
+        // style={{ background: `url(${background})` }}
+        className="bannerSection"
+      ></section>
+      <section className="formSection">
+        <h1
+          style={{
+            color: "#4C2B21",
+            // fontFamily: "Playfair display",
+            fontSize: "30px",
+          }}
+        >
+          Login
+        </h1>
+        <form className="login-form" onSubmit={submitHandler}>
+          <TextInput
+            type="email"
+            name="email"
+            label="Email"
+            value={formData.email}
+            onChange={changeHandler}
+          />
+          <TextInput
+            type="password"
+            name="password"
+            label="Password"
+            value={formData.password}
+            onChange={changeHandler}
+          />
+
+          <CustomButton type="submit" name="Login" />
+        </form>
+        <div>
+          <span className="nav-link" onClick={handleForgotPassword}>
+            Forget Password?
+          </span>
+        </div>
+        <div style={{ display: "flex", gap: 8 }}>
+          Don't have an account?
+          <span className="nav-link" onClick={() => navigate("/signup")}>
+            Signup
+          </span>
+        </div>
+      </section>
+    </div>
+  );
 };
 
-export default login;
+export default Login;
