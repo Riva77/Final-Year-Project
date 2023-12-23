3ea06df0dc94deb7c414bf51a215c8b2991aa6e7
Riva Karki <rivakarki86@gmail.com>
2023-12-23 11:33:55 +0545

    Updated App.jsx and main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
 import Signup from "./pages/login/signup";
+import Shop from "./pages/product/shop";
 import { Toaster } from "sonner";
+import Navbar from "./pages/navigation/Navbar";
+import Home from "./pages/home/home";
+
 function App() {
   return (
     <div>
       <Toaster richColors={true} />
+      <Navbar />
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/shop" element={<Shop />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </div>
   );
