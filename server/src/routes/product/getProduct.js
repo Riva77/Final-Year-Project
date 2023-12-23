8b30d9dab70750d056f7de840eae0bfb58fece3e
Riva Karki <rivakarki86@gmail.com>
2023-12-23 11:25:54 +0545

    Added product routes

---
+++ server/src/routes/product/getProduct.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const { Product } = require("../../models/Product.js"); //module import gareko
+
+router.get("/", async (req, res) => {
+  try {
+    const product = await Product.find();
+    if (!product) {
+      return res.status(404).send({ message: "Product not found" });
+    }
+
+    return res.status(200).json(product);
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
