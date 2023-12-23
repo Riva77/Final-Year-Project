d03703568e766570f50e4921a5e864d49f04f082
Riva Karki <rivakarki86@gmail.com>
2023-12-23 11:33:11 +0545

    Added index.js of component

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import CustomButton from "./buttons/CustomButton";
+import TextInput from "./inputField/TextInput";
+import RangeSlider from "./sliderAndDropdown/RangeSlider";
+import Dropdown from "./sliderAndDropdown/Dropdown";
+
+export { CustomButton, TextInput, RangeSlider, Dropdown };
