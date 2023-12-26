45aaaf848207df1f70a2fccfb985570c7aca62ca
Riva Karki <rivakarki86@gmail.com>
2023-12-26 13:49:55 +0545

    Added cloudinary and updated index.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,8 @@ const addAuthorRoutes = require("./routes/author/addAuthor.js");
 const getAuthorRoutes = require("./routes/author/getAuthor.js");
 const addGenreRoutes = require("./routes/genre/addGenre.js");
 const getGenreRoutes = require("./routes/genre/getGenre.js");
+const cloudinaryRoutes = require("../src/routes/cloudinary/cloudinary.js");
+const fileUpload = require("express-fileupload");
 
 connection();
 const app = express();
@@ -26,6 +28,8 @@ const corsOptions = {
 app.use(helmet());
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb" }));
+app.use(fileUpload({ useTempFiles: true }));
 
 app.use("/api/login", loginRoutes);
 app.use("/api/register", registerRoutes);
@@ -35,6 +39,7 @@ app.use("/api/addAuthor", addAuthorRoutes);
 app.use("/api/getAuthor", getAuthorRoutes);
 app.use("/api/addGenre", addGenreRoutes);
 app.use("/api/getGenre", getGenreRoutes);
+app.use("/api/cloudinary", cloudinaryRoutes);
 
 app.listen(8000, () => {
   //setting up the server to listen on the port 8000
