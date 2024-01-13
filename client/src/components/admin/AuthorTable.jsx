a37fd5ed147a729a8ad24343b93a35d0a1cb2825
Riva Karki <rivakarki86@gmail.com>
2024-01-13 11:33:49 +0545

    Created AuthorTable

--- client/src/components/admin/AuthorTable.jsx
+++ client/src/components/admin/AuthorTable.jsx
@@ -22,7 +22,7 @@ const AuthorTable = ({ items }) => {
                 </td>
 
                 <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-center">
-                  {item.author}
+                  {item.name}
                 </td>
               </tr>
             );
