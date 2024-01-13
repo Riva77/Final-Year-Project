3f18bf72e1bc34e31afb3b39ffc78e99a79ff655
Riva Karki <rivakarki86@gmail.com>
2024-01-13 11:35:51 +0545

    Updated Author.jsx

--- client/src/pages/admin/contents/Author.jsx
+++ client/src/pages/admin/contents/Author.jsx
@@ -1,24 +1,44 @@
-import { useState, useMemo } from "react";
-import { useSelector } from "react-redux";
+import { useState, useMemo, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AuthorTable from "../../../components/admin/AuthorTable";
 import CustomButton from "../../../components/buttons/CustomButton";
+import { setAuthorModal } from "../../../features/modalSlice";
+import { getAuthor } from "../../../apis/Author/getAuthor";
 
 const  Author = () => {
-  const productData = useSelector((state) => state.product.data);
+
   const [searchQuery, setSearchQuery] = useState("");
+  const [author, setAuthor] = useState(null);
+
+  const fetchAuthor = async () => {
+    const authorData = await getAuthor();
+    setAuthor(authorData?.data);
+  };
+
+  useEffect(() => {
+    fetchAuthor();
+  }, []);
+
+
 
   // Filter products based on the search query
-  const filteredProducts = useMemo(() => {
-    return productData?.filter((product) =>
-      product.author.toLowerCase().includes(searchQuery.toLowerCase())
+  const filteredAuthor = useMemo(() => {
+    return author?.filter((data) =>
+      data.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
-  }, [productData, searchQuery]);
+  }, [author, searchQuery]);
+
+
+  const dispatch = useDispatch();
+  const handleAddAuthor = () => {
+    dispatch(setAuthorModal());
+  };
 
   return (
     <div className="w-full my-2 flex flex-col gap-4">
-      <h1 className="font-bold text-xl mb-9"> Author Details</h1>
+      <h1 className="flex text-xl font-medium border-b-2 border-b-gray-200 mb-5 pb-2 items-center"> Author Details</h1>
 
-      <CustomButton type="Add" name="Add  Author" />
+      <CustomButton type="Add" name="Add  Author"  onClick={handleAddAuthor} />
       
       {/* Search Bar */}
       <input
@@ -32,7 +52,7 @@ const  Author = () => {
       
       
       {/* Pass filtered products to the Table component */}
-      < AuthorTable items={filteredProducts} />
+      < AuthorTable items={filteredAuthor} />
     </div>
   );
 };
