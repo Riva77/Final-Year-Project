eeedf20034c3ea9f0216a5ba67d213638b3c26ac
Riva Karki <rivakarki86@gmail.com>
2024-01-14 23:09:49 +0545

    Updated home.jsx

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CustomButton } from "../../components";
 import "../../App.css";
-import homeFive from "../../assets/homeFive.jpg";
+import colleen from "../../assets/colleen.png";
+import jenny from "../../assets/jenny.png";
+import ana from "../../assets/ana.png";
+import lauren from "../../assets/lauren.png";
+import tahereh from "../../assets/tahereh.png";
+import ProductCard from "../../components/card/ProductCard";
+import { useSelector } from "react-redux";
 
 const home = () => {
+  const productData = useSelector((state) => state.product.data);
+
+  const books = productData?.map((book) => {
+    return (
+      <ProductCard
+        name={book.name}
+        price={book.price}
+        image={book.image}
+        key={book._id}
+        onClick={() => handleProductClick(book._id)}
+      />
+    );
+  });
+
   return (
     <div>
       <section className="home" style={styles.wallpaper}>
@@ -33,6 +53,58 @@ const home = () => {
           </div>
         </div>
         <div style={styles.rightDiv}></div>
+      </section>
+      <section style={styles.specialSection}>
+        <div style={styles.specialDiv}>
+          <span style={styles.special}>What's so special about us?</span>
+        </div>
+        <div style={styles.aboutusDiv}>
+          <p style={styles.aboutUs}>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </div>
+      </section>
+      <section style={styles.toppickSection}>
+        <div style={styles.topPick}>Top Picks for you</div>
+        <div style={styles.booksPic}>{books}</div>
+        {/* <div style={styles.booksPic}>{books}</div> */}
+      </section>
+
+      <section style={styles.authorSection}>
+        <div style={styles.authorTittle}>Choose By Author</div>
+        <div style={styles.authorDiv}>
+          <div style={styles.eachauthorDiv}>
+            <img src={colleen} alt="collen" style={styles.authorImg} />
+            <span style={styles.authorName}>Colleen Hoover</span>
+          </div>
+          <div style={styles.eachauthorDiv}>
+            <img src={lauren} alt="lauren" style={styles.authorImg} />
+            <span style={styles.authorName}>Lauren Asher</span>
+          </div>
+          <div style={styles.eachauthorDiv}>
+            <img src={ana} alt="ana" style={styles.authorImg} />
+            <span style={styles.authorName}>Ana Huang</span>
+          </div>
+          <div style={styles.eachauthorDiv}>
+            <img src={jenny} alt="jenny" style={styles.authorImg} />
+            <span style={styles.authorName}>Jenny Han</span>
+          </div>
+          <div style={styles.eachauthorDiv}>
+            <img src={tahereh} alt="jenny" style={styles.authorImg} />
+            <span style={styles.authorName}>Tahereh Mafi</span>
+          </div>
+        </div>
       </section>
     </div>
   );
@@ -82,5 +154,111 @@ const styles = {
 
   rightDiv: {
     marginLeft: "auto",
+  },
+  specialSection: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    // height: "700px",
+    color: "#FAF0E6",
+    marginTop: "60px",
+  },
+  specialDiv: {
+    fontFamily: "Playfair Display",
+    display: "flex",
+    justifyContent: "center",
+    fontSize: "40px",
+    fontWeight: "bold",
+    padding: "30px 70px",
+    color: "#4C2B21",
+    gap: 100,
+    // marginBottom: "600px",
+  },
+  special: {
+    display: "flex",
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  aboutusDiv: {
+    background: "#FDFBF7",
+    boxShadow: "0 10px 14px rgba(0, 0, 0, 0.4)",
+    height: "400px",
+    width: "900px",
+  },
+
+  aboutUs: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Playfair Display",
+    fontSize: "20px",
+    fontWeight: "bold",
+    color: "black",
+    padding: "50px",
+    textAlign: "justify",
+  },
+
+  booksPic: {
+    gap: 200,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    padding: "40px",
+  },
+
+  toppickSection: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    marginTop: "60px",
+    gap: 50,
+  },
+  topPick: {
+    fontFamily: "Playfair Display",
+    fontSize: "40px",
+    fontWeight: "bold",
+    color: "#4C2B21",
+  },
+
+  authorSection: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    marginTop: "60px",
+    gap: 50,
+  },
+
+  authorTittle: {
+    fontFamily: "Playfair Display",
+    fontSize: "40px",
+    fontWeight: "bold",
+    color: "#4C2B21",
+    display: "flex",
+  },
+
+  authorDiv: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 100,
+  },
+
+  eachauthorDiv: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+
+  authorImg: { height: "160,", width: "113px", borderRadius: "75px" },
+  authorName: {
+    fontFamily: "Playfair display",
+    fontWeight: "bold",
   },
 };
