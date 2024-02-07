ef2082db4df98cb8bed5752be7462986f84be405
Riva Karki <rivakarki86@gmail.com>
2024-02-07 22:02:25 +0545

    Updated

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authslice";
+import authReducer from "../features/authSlice";
 import productReducer from "../features/productSlice";
 import modalReducer from "../features/modalSlice";
+import cartReducer from "../features/cartSlice";
 
 export const store = configureStore({
   reducer: {
     user: authReducer,
     product: productReducer,
     modal: modalReducer,
+    cart: cartReducer,
   },
 });
