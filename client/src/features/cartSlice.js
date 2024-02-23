738fdb8512b363420a5ed51cc5d1e43291d21dbb
Riva Karki <rivakarki86@gmail.com>
2024-02-23 21:53:58 +0545

    Updated cartSlice

---
+++ client/src/features/cartSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toastError, toastSuccess } from "../utils/toast";
+
+const localCart = JSON.parse(localStorage.getItem("cartItems"));
+
+const initialState = {
+  products: localCart ? localCart : [],
+};
+
+export const cartSlice = createSlice({
+  name: "Cart",
+  initialState,
+  reducers: {
+    addProductToCart: (state, action) => {
+      const product = action.payload;
+      const productIndex = state.products.findIndex(
+        (products) => products._id === product._id
+      );
+
+      if (productIndex >= 0) {
+        toastError("Product already in cart!");
+      } else {
+        state.products = [...state.products, product];
+        localStorage.setItem("cartItems", JSON.stringify(state.products));
+        toastSuccess("Product added to cart!");
+      }
+     
+    },
+
+    removeProductFromCart: (state, action) => {
+      const productId = action.payload;
+      state.products = state.products.filter(
+        (product) => product._id !== productId
+      );
+      localStorage.setItem("cartItems", JSON.stringify(state.products));
+    },
+    updateProductQuantity: (state, action) => {
+      console.log(action.payload);
+      const { productId, quantity } = action.payload;
+
+      const productIndex = state.products.findIndex(
+        (product) => product._id === productId
+      );
+
+      if (productIndex >= 0) {
+        state.products[productIndex].quantity = quantity;
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.products));
+    },
+  },
+});
+
+export const {
+  addProductToCart,
+  removeProductFromCart,
+  updateProductQuantity,
+} = cartSlice.actions;
+export default cartSlice.reducer;
