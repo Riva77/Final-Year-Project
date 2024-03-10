cdcef356f29c44f71d6e410ef4a208e82cfae171
Riva Karki <rivakarki86@gmail.com>
2024-03-10 22:48:38 +0545

    Added favorites

--- client/src/pages/profile/components/Favourites.jsx
+++ client/src/pages/profile/components/Favourites.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ProductCard from "../../../components/card/ProductCard";
 
 const Favourites = () => {
-  return <div>Favourites</div>;
+  const favourites = useSelector((state) => state.user?.data?.favoriteBooks);
+  const navigate = useNavigate();
+
+  const handleProductClick = (productId) => {
+    navigate(`/shop/productDetails/${productId}`);
+  };
+
+  return (
+    <div
+      style={{
+        gap: 50,
+        display: "flex",
+        justifyContent:
+          favourites?.length >= 4 ? "space-between" : "flex-start",
+        flexWrap: "wrap",
+        margin: "20px",
+      }}
+    >
+      {favourites?.map((book) => {
+        return (
+          <ProductCard
+            name={book.name}
+            price={book.price}
+            image={book.image}
+            key={book._id}
+            onClick={() => handleProductClick(book._id)}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Favourites;
