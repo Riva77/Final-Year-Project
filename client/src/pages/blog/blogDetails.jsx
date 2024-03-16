d09342f0cc79610936d20c3e3e4fc2a9ac8ae714
Riva Karki <rivakarki86@gmail.com>
2024-03-16 21:43:44 +0545

    Updated blogDetails

--- client/src/pages/blog/blogDetails.jsx
+++ client/src/pages/blog/blogDetails.jsx
@@ -1,43 +1,78 @@
 import { useEffect, useState } from "react";
-import { getPost } from "../../apis/blog/getPost";
+import { getSinglePost } from "../../apis/blog/getSinglePost";
 import IndividualBlogCard from "../../components/card/IndividualBlogCard";
-
+import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
- 
-const [blogData, setBlogData] = useState(null);
+  const { blogId } = useParams();
 
-const fetchBlogData= async()=>{
-    const blog = await getPost();
+  const [blogData, setBlogData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const blog = await getSinglePost(blogId);
     setBlogData(blog);
-}
+  };
 
-useEffect(()=>{
+  useEffect(() => {
     fetchBlogData();
-},[]);
+    console.log(blogData?.content);
+  }, []);
 
-const blogs = blogData?.map((blog) => {
-    //blogdata xa vani matra map garni (?)
-    return (
-      <IndividualBlogCard
-        image={blog.image}
-        title={blog.title}
-        summary={blog.summary}
-        content={blog.content}
-        time={blog.createdAt}
-        key={blog._id}
-
-      />
-    );
-  });
+  // const blogs = blogData?.map((blog) => {
+  //   //blogdata xa vani matra map garni (?)
+  //   return (
+  //     <IndividualBlogCard
+  //       image={blog.image}
+  //       title={blog.title}
+  //       summary={blog.summary}
+  //       content={blog.content}
+  //       time={blog.createdAt}
+  //       key={blog._id}
+  //     />
+  //   );
+  // });
 
   return (
     <div>
       <section style={styles.outerSection}>
         <section style={styles.innerSection}>
-        
-        <div>{blogs}</div>
+          <div>
+            {/* <div>
+              <article className=" w-full overflow-hidden rounded-lg shadow transition hover:shadow-lg"> */}
+            <div className="w-full rounded-lg shadow transition hover:shadow-lg">
+              <article className="w-full">
+                <img
+                  alt="Image"
+                  src={blogData?.image}
+                  className="h-96 w-full object-cover"
+                />
 
+                <div className="h-full bg-white p-4 sm:p-6 w-full flex flex-col gap-3">
+                  <time
+                    datetime="2022-10-10"
+                    className="block text-xs text-gray-500"
+                  >
+                    {new Date(blogData?.createdAt).toLocaleString()}
+                  </time>
+
+                  <a href="#">
+                    <h3 className="mt-0.5 text-2xl text-[#4C2B21]">
+                      {blogData?.title}
+                    </h3>
+                  </a>
+
+                  <p className="mt-2  text-sm/relaxed text-[#7b351f] font-medium">
+                    {blogData?.summary}
+                  </p>
+                  <p
+                    className="h-full mt-2  text-sm/relaxed text-gray-500"
+                    style={{ textAlign: "justify" }}
+                    dangerouslySetInnerHTML={{ __html: blogData?.content }}
+                  ></p>
+                </div>
+              </article>
+            </div>
+          </div>
         </section>
       </section>
     </div>
@@ -65,12 +100,5 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     gap: 30,
-  }
-
- 
-  
-  
-
-
-
+  },
 };
