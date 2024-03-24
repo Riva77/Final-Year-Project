cb23ef8c090dccc3e82e6555f1d11bfa578dbecb
Riva Karki <rivakarki86@gmail.com>
2024-03-24 22:25:28 +0545

    Added slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,6 +3,7 @@ import authReducer from "../features/authSlice";
 import productReducer from "../features/productSlice";
 import modalReducer from "../features/modalSlice";
 import cartReducer from "../features/cartSlice";
+import topProductReducer from "../features/topProductSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     product: productReducer,
     modal: modalReducer,
     cart: cartReducer,
+    topProduct: topProductReducer,
   },
 });
