182560ddbe53e761a8e0a1ca3f97c181a95091a4
Riva Karki <rivakarki86@gmail.com>
2024-04-17 22:11:44 +0545

    Updated createOrder

--- server/src/routes/order/createOrder.js
+++ server/src/routes/order/createOrder.js
@@ -5,25 +5,35 @@ const { khaltiPayment } = require("../../services/payment.service.js");
 router.post("/", async (req, res) => {
   //post method banako. backemd ma database ma data pathauna frontend bata aako data
   let body = { ...req.body }; //body ma chai frontend bata aako data store gareko
+  console.log(body);
 
   try {
     const order = await new Order({ ...body }).save(); //backend ma data entry gareko
 
-    //khalti payload
-    const requestPayload = {
-      return_url: `http://localhost:8000/api/khalti/callback`,
-      website_url: `http://localhost:8000`,
-      amount: req.body.totalPrice * 100,
-      purchase_order_id: order._id,
-      purchase_order_name: "Order " + order.customer,
-    };
+    if (body.paymentType === "Khalti") {
+      //khalti payload
+      const requestPayload = {
+        return_url: `http://localhost:8000/api/khalti/callback`,
+        website_url: `http://localhost:8000`,
+        amount: req.body.totalPrice * 100,
+        purchase_order_id: order._id,
+        purchase_order_name: "Order " + order.customer,
+      };
 
-    const khalti = await khaltiPayment(requestPayload); //khalti payment gareko
-    console.log(khalti);
-
-    res
-      .status(201)
-      .send({ data: khalti, message: "Order placed successfully." });
+      const khalti = await khaltiPayment(requestPayload); //khalti payment gareko
+      res.status(200).send({
+        data: khalti,
+        order_id: order._id,
+        type: "khalti",
+        message: "Redirecting to khalti.",
+      });
+    } else {
+      res.status(200).send({
+        data: order,
+        type: "cash",
+        message: "Order placed successfully.",
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Internal server error." });
