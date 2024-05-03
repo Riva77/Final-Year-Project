f2ab579f0098e07c7ebbd258a18b90bf5958f49f
Riva Karki <rivakarki86@gmail.com>
2024-05-03 21:06:24 +0545

    Updated updateProduct modal

---
+++ client/src/apis/product/updateProduct.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+export const updateProduct = async (productId, {
+  name,
+  price,
+  description,
+  quantity,
+  image,
+  author,
+  synopsis,
+  genre,
+  pages,
+}) => {
+  const API_URL = "http://localhost:8000/api";
+  try {
+    const response = await axios.put(`${API_URL}/updateProduct/${productId}`, {
+      name,
+      price,
+      description,
+      quantity,
+      image,
+      author,
+      synopsis,
+      genre,
+      pages,
+    });
+
+    const data = response?.data; // if response exists then get response data
+    return { success: true, data };
+  } catch (error) {
+    if (
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status <= 500
+    ) {
+      return {
+        success: false,
+        error: error.response.data.message || error.response.data,
+      };
+    }
+  }
+};
