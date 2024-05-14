c3757aba420ac7edcab71b0be38f350794f0df5e
Riva Karki <rivakarki86@gmail.com>
2024-05-14 21:39:42 +0545

    Updated invoice

--- client/src/pages/profile/components/Invoice.jsx
+++ client/src/pages/profile/components/Invoice.jsx
@@ -5,11 +5,13 @@ import {
   Text,
   View,
   StyleSheet,
+  Image,
 } from "@react-pdf/renderer";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { formatDate } from "../../../utils/helper";
 import Spinner from "../../../components/spinner/spinner";
 import axios from "axios";
+import logo from "../../../assets/logo.png";
 
 const Invoice = () => {
   const orderId = new URLSearchParams(window.location.search).get("orderId");
@@ -63,9 +65,9 @@ const Invoice = () => {
             " " +
             orderDetails?.customer?.lastName}
         </div>
-        <div className="text-gray-700 mb-2">
+        {/* <div className="text-gray-700 mb-2">
           {orderDetails?.address + " - " + orderDetails?.wardNumber}
-        </div>
+        </div> */}
         <div className="text-gray-700 mb-2">{orderDetails?.district}</div>
         <div className="text-gray-700">{orderDetails?.customer?.email}</div>
       </div>
@@ -130,42 +132,70 @@ const Invoice = () => {
 const InvoicePDF = ({ orderDetails }) => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <Text style={styles.title}>Books & R Invoice</Text>
-      <View style={{ marginBottom: 10 }}>
-        <Text style={styles.text}>
-          Date: {formatDate(orderDetails?.orderDate)}
-        </Text>
-        <Text style={styles.text}>Order Id: {orderDetails?._id}</Text>
+      <Image style={styles.logo} src={logo} />
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.reportTitle}>Order Invoice</Text>
       </View>
-      <View style={{ marginBottom: 20 }}>
-        <Text style={styles.text}>Bill To:</Text>
-        <Text style={styles.text}>
+
+      <Fragment>
+        <View style={styles.invoiceNoContainer}>
+          <Text style={styles.label}>Invoice No:</Text>
+          <Text style={styles.invoiceDate}>{orderDetails._id}</Text>
+        </View>
+        <View style={styles.invoiceDateContainer}>
+          <Text style={styles.label}>Date: </Text>
+          <Text>{formatDate(orderDetails?.orderDate)}</Text>
+        </View>
+      </Fragment>
+
+      <View style={styles.headerContainer}>
+        <Text style={styles.billTo}>Bill To:</Text>
+        <Text>
           {orderDetails?.customer?.firstName +
             " " +
             orderDetails?.customer?.lastName}
         </Text>
-        <Text style={styles.text}>
-          {orderDetails?.address + " - " + orderDetails?.wardNumber}
-        </Text>
-        <Text style={styles.text}>{orderDetails?.district}</Text>
-        <Text style={styles.text}>{orderDetails?.customer?.email}</Text>
-      </View>
-      <View style={{ display: "flex", justifyContent: "space-between" }}>
-        <Text style={styles.text}>Product</Text>
-        <Text style={styles.text}>Amount</Text>
-      </View>
-      <View style={{ marginBottom: 10 }}>
-        <Text style={styles.text}>Product 1</Text>
-        <Text style={styles.text}>$100.00</Text>
+
+        <Text>{orderDetails?.phoneNumber}</Text>
+        <Text>{orderDetails?.customer.email}</Text>
       </View>
 
-      <View>
-        <Text style={styles.text}>Total</Text>
-        <Text style={styles.text}></Text>
-        <Text style={styles.text}>$225.00</Text>
+      <View style={styles.tableContainer}>
+        <View style={styles.container}>
+          <Text style={styles.description}>Item Description</Text>
+          <Text style={styles.qty}>Quantity</Text>
+          <Text style={styles.rate}>Unit Price</Text>
+          <Text style={styles.amount}>Amount</Text>
+        </View>
+
+        <Fragment>
+          {orderDetails?.products.map((item, index) => (
+            <View style={rowStyles.row} key={index}>
+              <Text style={rowStyles.description}>{item.product.name}</Text>
+              <Text style={rowStyles.qty}>{item.quantity}</Text>
+              <Text style={rowStyles.rate}>{item.product.price}</Text>
+              <Text style={rowStyles.amount}>
+                Rs.{(item.quantity * item.product.price).toFixed(2)}
+              </Text>
+            </View>
+          ))}
+        </Fragment>
+
+        {/* <InvoiceTableBlankSpace
+          rowsCount={tableRowsCount - invoice.items.length}
+        /> */}
+
+        <View style={footerStyles.row}>
+          <Text style={footerStyles.description}>TOTAL</Text>
+          <Text style={footerStyles.total}>
+            Rs.{orderDetails?.totalPrice.toFixed(2)}
+          </Text>
+        </View>
       </View>
-      <View style={{ marginTop: 20 }}>
-        <Text style={styles.text}>Thank you for your order!</Text>
+
+      <View style={thankStyles.titleContainer}>
+        <Text style={thankStyles.reportTitle}>Thank you for your order.</Text>
       </View>
     </Page>
   </Document>
@@ -175,39 +205,169 @@ export default Invoice;
 
 const styles = StyleSheet.create({
   page: {
+    fontFamily: "Helvetica",
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    lineHeight: 1.5,
     flexDirection: "column",
-    backgroundColor: "#fff",
-    padding: 20,
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-    textAlign: "center",
+  logo: {
+    width: 74,
+    height: 66,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+
+  titleContainer: {
+    flexDirection: "row",
+    marginTop: 24,
+  },
+  reportTitle: {
     color: "#4C2B21",
+    letterSpacing: 4,
+    fontSize: 25,
+    textAlign: "center",
+    textTransform: "uppercase",
   },
-  text: {
+  invoiceNoContainer: {
+    flexDirection: "row",
+    marginTop: 36,
+    justifyContent: "flex-end",
+  },
+  invoiceDateContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  invoiceDate: {
     fontSize: 12,
-    marginBottom: 10,
+    fontStyle: "bold",
   },
-  table: {
-    width: "100%",
-    border: "1px solid #000",
-    borderCollapse: "collapse",
+  label: {
+    width: 60,
   },
-  tableHeader: {
-    backgroundColor: "#f2f2f2",
-    border: "1px solid #000",
-    padding: 8,
+  headerContainer: {
+    marginTop: 36,
   },
-  tableRow: {
-    border: "1px solid #000",
-    padding: 8,
+  billTo: {
+    marginTop: 20,
+    paddingBottom: 3,
+    fontFamily: "Helvetica-Oblique",
   },
-  tableHeaderCell: {
-    fontWeight: "bold",
+  tableContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 24,
+    borderWidth: 1,
+    borderColor: "#4C2B21",
+  },
+
+  container: {
+    flexDirection: "row",
+    borderBottomColor: "#4C2B21",
+    backgroundColor: "#4C2B21",
+    borderBottomWidth: 1,
+    alignItems: "center",
+    height: 24,
+    color: "white",
+    textAlign: "center",
+    fontStyle: "bold",
+    flexGrow: 1,
+  },
+  description: {
+    width: "60%",
+    borderRightColor: "#4C2B21",
+    color: "white",
+    borderRightWidth: 1,
+  },
+  qty: {
+    width: "10%",
+    borderRightColor: "#4C2B21",
+    color: "white",
+    borderRightWidth: 1,
+  },
+  rate: {
+    width: "15%",
+    borderRightColor: "#4C2B21",
+    color: "white",
+    borderRightWidth: 1,
+  },
+  amount: {
+    width: "15%",
+    color: "white",
+  },
+});
+
+const rowStyles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    borderBottomColor: "#4C2B21",
+    borderBottomWidth: 1,
+    alignItems: "center",
+    height: 24,
+    fontStyle: "bold",
+  },
+  description: {
+    width: "60%",
     textAlign: "left",
+    borderRightColor: "#4C2B21",
+    borderRightWidth: 1,
+    paddingLeft: 8,
   },
-  tableCell: {
+  qty: {
+    width: "10%",
+    borderRightColor: "#4C2B21",
+    borderRightWidth: 1,
     textAlign: "right",
+    paddingRight: 8,
+  },
+  rate: {
+    width: "15%",
+    borderRightColor: "#4C2B21",
+    borderRightWidth: 1,
+    textAlign: "right",
+    paddingRight: 8,
+  },
+  amount: {
+    width: "15%",
+    textAlign: "right",
+    paddingRight: 8,
+  },
+});
+
+const footerStyles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    borderBottomColor: "#4C2B21",
+    borderBottomWidth: 1,
+    alignItems: "center",
+    height: 24,
+    fontSize: 12,
+    fontStyle: "bold",
+  },
+  description: {
+    width: "85%",
+    textAlign: "right",
+    borderRightColor: "#4C2B21",
+    borderRightWidth: 1,
+    paddingRight: 8,
+  },
+  total: {
+    width: "15%",
+    textAlign: "right",
+    paddingRight: 8,
+  },
+});
+
+const thankStyles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    marginTop: 12,
+  },
+  reportTitle: {
+    fontSize: 12,
+    textAlign: "center",
+    textTransform: "uppercase",
   },
 });
